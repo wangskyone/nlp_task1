ae7a60de6b70eaad6690b9efb04a42cd72741d78
liyu_UCAS <87369926+fcddcdxA@users.noreply.github.com>
2024-04-19 17:02:07 +0800

    Add files via upload

--- clip_demo/img_ch_dataset.py
+++ clip_demo/img_ch_dataset.py
@@ -1,6 +1,12 @@
+import torch
 from PIL import Image
+from tqdm import tqdm
+
 from cn_clip import clip
+from cn_clip.clip import load_from_name
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from sklearn.metrics import accuracy_score
 
 
 def get_img_ch(img_ch_dir=r'./data/', mode='train'):
@@ -9,7 +15,7 @@ def get_img_ch(img_ch_dir=r'./data/', mode='train'):
     :param mode: train,valid or test
     :return:the list of image path and the corresponding character
     """
-    index_file_path = img_ch_dir + mode + '/' + 'img_ch.txt'
+    index_file_path = img_ch_dir + mode + '/' + 'index_img.txt'
     f = open(index_file_path, mode='r', encoding='utf-8')
     data = f.readlines()
     f.close()
@@ -17,8 +23,12 @@ def get_img_ch(img_ch_dir=r'./data/', mode='train'):
     character = []
     for item in data:
         item = item.strip().split('\t')
-        imge_path.append(img_ch_dir + mode + '/' + item[0])
-        character.append(item[1])
+        if len(item) == 2:
+            imge_path.append(img_ch_dir + mode + '/' + item[0].split('/')[-1])
+            character.append(item[1])
+        else:
+            imge_path.append(img_ch_dir + mode + '/' + item[0].split('/')[-1])
+            character.append('')
     return imge_path, character
 
 
@@ -45,14 +55,62 @@ def get_data_loader(process, batch_size=1, img_ch_dir=r'./data/', mode='train'):
     image_path, character = get_img_ch(img_ch_dir, mode=mode)
     data_set = Img_Ch_Dataset(image_path, character, process=process)
     if mode == 'train':
-        data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=4)
+        data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=4,
+                                 drop_last=False)
     else:
-        data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=4)
+        data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=False, pin_memory=False, num_workers=4,
+                                 drop_last=False)
     return data_loader
 
 
+def get_all_valid_words(valid_index_path=r'data/valid/index_img.txt'):
+    f = open(valid_index_path, mode='r', encoding='utf-8')
+    data = f.readlines()
+    f.close()
+    valid_words = []
+    for item in data:
+        item = item.strip().split('\t')
+        if len(item) == 2:
+            item = item[1]
+        else:
+            item = ''
+        if item not in valid_words:
+            valid_words.append(item)
+    return valid_words
+
+
+def get_valid_word_index_list(img_ch_dir):
+    valid_imgs, valid_words = get_img_ch(img_ch_dir=img_ch_dir, mode='valid')
+    all_valid_words = get_all_valid_words()
+    valid_index_list = []
+    for item in valid_words:
+        valid_index_list.append(all_valid_words.index(item))
+    return valid_index_list, all_valid_words
+
+
+def valid_method(img_ch_dir, device, clip_net, process):
+    valid_index_list, all_valid_words = get_valid_word_index_list(img_ch_dir=img_ch_dir)
+    valid_data_loader = get_data_loader(process=process, batch_size=64, img_ch_dir=img_ch_dir, mode='valid')
+    valid_token = clip.tokenize(all_valid_words).to(torch.device(device))
+    valid_tensors = clip_net.encode_text(valid_token)
+    sim_results = []
+    for idx, data in enumerate(tqdm(valid_data_loader)):
+        imgs = data[0].to(device)
+        with torch.no_grad():
+            imgs_tensor = clip_net.encode_image(imgs)
+            imgs_tensor = imgs_tensor / imgs_tensor.norm(dim=-1, keepdim=True)
+            sim_mtrix = F.cosine_similarity(valid_tensors.unsqueeze(0), imgs_tensor.unsqueeze(1), dim=-1)
+            sim_result = torch.argmax(sim_mtrix, dim=-1).cpu().numpy().tolist()
+            sim_results += sim_result
+    acc = accuracy_score(valid_index_list, sim_results)
+    return acc
+
+
 if __name__ == '__main__':
-    data_loader = get_data_loader()
-    for img, token in data_loader:
-        print(img)
-        print(token)
+    # valid_words = get_all_valid_words()
+    # valid_token = clip.tokenize(valid_words).to(torch.device('cuda:0'))
+    # clip_net, process = load_from_name(name='RN50', device=torch.device('cuda:0'), download_root='./clip_model')
+    # valid_tensors = clip_net.encode_text(valid_token)
+    # print(valid_tensors.shape)
+    _, _, vlid_index = get_valid_word_index_list(img_ch_dir=r'./data/')
+    print(vlid_index)
--- clip_demo/train_clip_demo.py
+++ clip_demo/train_clip_demo.py
@@ -1,12 +1,14 @@
 import argparse
+
 import torch
-from tqdm import tqdm
-from cn_clip.clip import load_from_name
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from img_ch_dataset import get_data_loader
 import torch.nn as nn
+import torch.optim as optim
 from loguru import logger
+from torch.optim import lr_scheduler
+from tqdm import tqdm
+
+from cn_clip.clip import load_from_name
+from img_ch_dataset import get_data_loader, get_all_valid_words, valid_method
 
 
 def clip_loss(img_tensor, ch_tensor, device):
@@ -22,7 +24,7 @@ def train(args):
     print(args)
     logger.info('start training!')
     device = torch.device(args.device)
-    clip_net, process = load_from_name(name=args.clip_img_head, device=device, download_root='./vit_model')
+    clip_net, process = load_from_name(name=args.clip_img_head, device=device, download_root='./clip_model')
     optimizer = optim.AdamW(clip_net.parameters(), lr=args.learning_rate, betas=args.betas, eps=args.eps,
                             weight_decay=0.001)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
@@ -33,7 +35,7 @@ def train(args):
         scheduler.step()
         batch_num = 0
         with torch.cuda.amp.autocast(enabled=True):
-            for idx, data in tqdm(enumerate(data_loader)):
+            for idx, data in tqdm(enumerate(tqdm(data_loader))):
                 imgs = data[0].to(device)
                 tokens = data[1].to(device)
                 batch_num += 1
@@ -41,13 +43,16 @@ def train(args):
                 with torch.set_grad_enabled(args.mode == 'train'):
                     imgs_tensor = clip_net.encode_image(imgs)
                     token_tensor = clip_net.encode_text(tokens)
-                    imgs_tensor /= imgs_tensor.norm(dim=-1, keepdim=True)
-                    token_tensor /= token_tensor.norm(dim=-1, keepdim=True)
+                    imgs_tensor = imgs_tensor / imgs_tensor.norm(dim=-1, keepdim=True)
+                    token_tensor = token_tensor / token_tensor.norm(dim=-1, keepdim=True)
                     loss = clip_loss(img_tensor=imgs_tensor, ch_tensor=token_tensor, device=args.device)
                     loss.backward()
                     optimizer.step()
                 if batch_num % args.valid_step == 0:
-                    logger.info('step:{},loss:{}'.format(str(batch_num), str(float(loss))))
+                    clip_net.eval()
+                    acc = valid_method(img_ch_dir=args.img_ch_dir, device=args.device, clip_net=clip_net,
+                                       process=process)
+                    logger.info('acc:{} in step:{},loss:{}'.format(str(acc),str(batch_num), str(float(loss))))
         torch.save(clip_net.state_dict(), args.model_dir + f'{args.clip_img_head}_epoch_{str(i)}.pth')
 
 
@@ -59,7 +64,7 @@ def main():
                         help='path to datasets')
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'valid', 'test'],
                         help='train,valid or test')
-    parser.add_argument('--batch_size', type=int, default=4, help='the size of a batch')
+    parser.add_argument('--batch_size', type=int, default=64, help='the size of a batch')
     parser.add_argument('--device', type=str, default='cuda:0', choices=['cuda:0', 'cpu'])
     parser.add_argument('--learning_rate', type=float, default=1e-06)
     parser.add_argument('--betas', type=tuple, default=(0.9, 0.98))
